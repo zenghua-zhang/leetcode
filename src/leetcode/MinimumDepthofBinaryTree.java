b8af47c133204431782f2c30bcc6da99d5dbf4e8
zenghua-zhang <zzhandy@gmail.com>
2015-01-07 15:32:04 +0800

    MinimumDepthofBinaryTree

---
+++ src/leetcode/MinimumDepthofBinaryTree.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class MinimumDepthofBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return min(root, 1, Integer.MAX_VALUE);
+
+    }
+
+    private int min(TreeNode root, int depth, int min) {
+        if (root.left == null & root.right == null) {
+            min = Math.min(depth, min);
+        } else {
+            if (root.left != null) {
+                min = Math.min(min, min(root.left, depth + 1, min));
+            }
+            if (root.right != null) {
+                min = Math.min(min, min(root.right, depth + 1, min));
+            }
+        }
+        return min;
+    }
+}
