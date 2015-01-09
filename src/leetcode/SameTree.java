055f80716df8d749ee2c5274e1af8ae512c92749
zenghua-zhang <zzhandy@gmail.com>
2015-01-09 14:26:56 +0800

    is same tree

---
+++ src/leetcode/SameTree.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((q == null && p != null) || (q != null && p == null)) {
+            return false;
+        }
+        if (q != null && p != null && q.val != p.val) {
+            return false ;
+        }
+
+        boolean ret = true;
+        ret = ret && isSameTree(p.left, q.left);
+        ret = ret && isSameTree(p.right, q.right);
+
+        return ret;
+    }
+}
