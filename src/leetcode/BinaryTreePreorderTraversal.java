3d7f955003704bdf16c10fd7923fdd8e40d5c709
zenghua-zhang <zzhandy@gmail.com>
2015-01-13 13:07:55 +0800

    preorder traversal

---
+++ src/leetcode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null){
+            return list;
+        }
+        LinkedList<TreeNode> v = new LinkedList<TreeNode>();
+        v.push(root);
+        
+        while(!v.isEmpty()){
+            TreeNode node = v.pop();
+            list.add(node.val);
+            if(node.right!=null){
+                v.push(node.right);
+            }
+            if(node.left != null){
+                v.push(node.left);
+            }
+        }
+        
+        return list;
+    }
+}
