a9db4a19088641a5e81d8c9a1bb801d0f73d1d20
zenghua-zhang <zzhandy@gmail.com>
2015-01-13 13:22:14 +0800

    removeNthFromEnd

---
+++ src/leetcode/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class RemoveNthNodeFromEndofList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        int nm = n;
+        int len = 0;
+        while (true) {
+            fast = fast.next;
+            len++;
+            if(fast == null){
+                break;
+            }
+            if (n > 0) {
+                n--;
+            } else {
+                slow = slow.next;
+            }
+        }
+        if (len == nm) {
+            if(len == 1){
+                return null;   
+            }else{
+                return head.next;
+            }
+        }
+        ListNode tmp = slow.next.next;
+
+        slow.next = tmp;
+
+        return head;
+    }
+}
