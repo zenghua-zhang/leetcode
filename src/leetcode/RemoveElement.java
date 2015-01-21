6344a8ce94fbe39107839b71fda3a66df8a276e6
zenghua-zhang <zzhandy@gmail.com>
2015-01-21 10:07:56 +0800

    remove element

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class RemoveElement {
+    public int removeElement(int[] A, int elem) {
+        int index = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != elem) {
+                A[index++] = A[i];
+            }
+        }
+        return index;
+    }
+}
