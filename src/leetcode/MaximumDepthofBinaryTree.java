dc82e90b84fb17ababa9240532e780c7312e3f81
zenghua.zhang <zenghua.zhang@hojonetwork.com>
2015-09-28 15:22:23 +0800

    Maximum Depth of Binary Tree

---
+++ src/leetcode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+}
