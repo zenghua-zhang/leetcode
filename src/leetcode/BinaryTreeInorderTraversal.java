50d7d26b8e3a9b6dd4bc2302cfd18f90a2cda10c
zenghua.zhang <zzhandy@gmail.com>
2016-04-11 11:29:19 +0800

    94. Binary Tree Inorder Traversal 迭代方式中序遍历

---
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList<Integer>();
+        Set<TreeNode> traveled = new HashSet<TreeNode>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode tmp = queue.peekLast();
+            if (tmp == null) {
+                queue.removeLast();
+                continue;
+            }
+            if (tmp.left == null || traveled.contains(tmp)) {
+                ret.add(tmp.val);
+                queue.removeLast();
+                queue.add(tmp.right);
+            } else {
+                traveled.add(tmp);
+                queue.add(tmp.left);
+            }
+
+        }
+
+        return ret;
+
+    }
+}
