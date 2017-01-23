cf66f2b7527a1dae2b1620a1c64a2a0ed851b4b4
Zenghua Zhang <zenghua.zhang@d1m.cn>
2017-01-23 16:23:06 +0800

    155. Min Stack

---
+++ src/leetcode/MinStack.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            Integer min = Math.min(minStack.peek(), x);
+            minStack.push(min);
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+
+    }
+
+    public int getMin() {
+        return minStack.peek();
+
+    }
+
+}
