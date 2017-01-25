2bb5d914a98dc1578e6589425a634c4f98fa2f89
Zenghua Zhang <zenghua.zhang@d1m.cn>
2017-01-25 17:03:58 +0800

    404. Sum of Left Leaves

---
+++ src/leetcode/SumOfLeftLeaves.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return helper(root, false);
+    }
+
+    private int helper(TreeNode root, boolean isleft) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return isleft ? root.val : 0;
+        }
+        return helper(root.left, true) + helper(root.right, false);
+    }
+}
