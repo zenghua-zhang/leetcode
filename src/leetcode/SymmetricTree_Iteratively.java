6fd9c06c73c22785488b333bdc7bddc8ba637f0d
Zenghua Zhang <zenghua.zhang@d1m.cn>
2017-02-22 15:30:59 +0800

    101. Symmetric Tree

---
+++ src/leetcode/SymmetricTree_Iteratively.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+public class SymmetricTree_Iteratively {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        LinkedList<TreeNode> leftList = new LinkedList<>();
+        LinkedList<TreeNode> rightList = new LinkedList<>();
+        leftList.push(root.left);
+        rightList.add(root.right);
+        while (!leftList.isEmpty() && !rightList.isEmpty()) {
+            TreeNode left = leftList.poll();
+            TreeNode right = rightList.poll();
+
+            if (null == left && null == right) {
+                continue;
+            }
+            if (null == left || null == right) {
+                return false;
+            }
+            if(left.val != right.val){
+                return false;
+            }
+            leftList.add(left.left);
+            leftList.add(left.right);
+            rightList.add(right.right);
+            rightList.add(right.left);
+        }
+
+        return true;
+    }
+}
---
+++ src/leetcode/SymmetricTree_Recursively.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class SymmetricTree_Recursively {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null && right != null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return helper(left.left, right.right) && helper(left.right, right.left);
+
+    }
+}
