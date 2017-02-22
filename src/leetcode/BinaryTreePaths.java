a7e0fdda406d871e4b0e13a32d0b9cf764c11694
Zenghua Zhang <zenghua.zhang@d1m.cn>
2017-02-22 13:48:46 +0800

    257. Binary Tree Paths

---
+++ src/leetcode/BinaryTreePaths.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+
+        if (root.left == null && root.right == null) {
+            ret.add(String.valueOf(root.val));
+        }
+
+        List<String> lefts = binaryTreePaths(root.left);
+        for (String str : lefts) {
+            ret.add(root.val + "->" + str);
+        }
+
+        List<String> rights = binaryTreePaths(root.right);
+        for (String str : rights) {
+            ret.add(root.val + "->" + str);
+        }
+
+        return ret;
+    }
+}
