878949905d9eaa5a9bd53c95cc269ba63e3badc8
zenghua.zhang <zzhandy@gmail.com>
2020-01-20 17:51:20 +0800

    981. Time Based Key-Value Store

---
+++ src/main/java/leetcode/TimeBasedKeyValueStore.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeMap.Entry;
+
+public class TimeBasedKeyValueStore {
+
+  class TimeMap {
+
+    TreeMap<TimeKey, String> map;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public TimeMap() {
+      map = new TreeMap<>();
+
+    }
+
+    public void set(String key, String value, int timestamp) {
+
+      map.put(new TimeKey(key, timestamp), value);
+
+    }
+
+    public String get(String key, int timestamp) {
+      TimeKey timeKey = map.floorKey(new TimeKey(key, timestamp));
+      if (timeKey == null) {
+        return "";
+      }
+      return map.get(timeKey);
+    }
+
+    private class TimeKey implements Comparable<TimeKey> {
+
+      private final String key;
+
+      private final Integer timestamp;
+
+      public TimeKey(String key, Integer timestamp) {
+        this.key = key;
+        this.timestamp = timestamp;
+      }
+
+
+      @Override
+      public int compareTo(TimeKey that) {
+        if (this.key.compareTo(that.key) < 0) {
+          return -1;
+        } else if (this.key.compareTo(that.key) > 0) {
+          return 1;
+        }
+
+        if (this.timestamp.compareTo(that.timestamp) < 0) {
+          return -1;
+        } else if (this.timestamp.compareTo(that.timestamp) > 0) {
+          return 1;
+        }
+        return 0;
+      }
+    }
+  }
+
+
+}
