fb5a726cf85294949e913103c6f458f9db3171b0
zenghua.zhang <zzhandy@gmail.com>
2020-01-22 14:22:20 +0800

    998. Maximum Binary Tree II

---
+++ src/main/java/leetcode/MaximumBinaryTreeII.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import model.TreeNode;
+
+public class MaximumBinaryTreeII {
+
+  public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+    if (root == null) {
+      return new TreeNode(val);
+    }
+    if (root.val < val) {
+      TreeNode node = new TreeNode(val);
+      node.left = root;
+      return node;
+    }
+    root.right = insertIntoMaxTree(root.right, val);
+
+    return root;
+  }
+}
